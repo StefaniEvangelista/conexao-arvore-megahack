221c6cc7d760fbfd2905f0eb4fcebe2f9627a4fc
Stefani Evangelista <stefanievangelistasilva@hotmail.com>
2020-07-05 12:01:48 -0300

    - incluindo component gincana-leitura - incluindo component ranking - criando rotas para os components criados - incluindo router link nos menus da tela home

---
+++ src/app/modules/tela-gincana-leitura/tela-gincana-leitura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-gincana-leitura',
+  templateUrl: './tela-gincana-leitura.component.html',
+  styleUrls: ['./tela-gincana-leitura.component.css']
+})
+export class TelaGincanaLeituraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
